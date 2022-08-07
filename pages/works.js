b04253ee83eccc921cb66231e8d5a75e2139d375
Temisaputra <temisaputra26@gmail.com>
2022-08-07 11:43:46 +0700

    Update

--- pages/works.js
+++ pages/works.js
@@ -2,8 +2,8 @@ import { Container, Heading, SimpleGrid } from "@chakra-ui/react";
 import Section from "../components/section";
 import Layout from '../components/layouts/article'	
 import { WorkGridItem } from "../components/grid-item";
-import thumbMyProject from '../public/images/works/penjualan_sepatu.png'
-import thumbMyProject2 from '../public/images/works/sistem_helpdesk.png'
+import MyProject from '../public/images/works/penjualan_sepatu.png'
+import MyProject2 from '../public/images/works/sistem_helpdesk.png'
 
 const Works = () => {
     return (
@@ -15,12 +15,12 @@ const Works = () => {
 
             <SimpleGrid columns={[1,1,2]} gap={6}>
                 <Section>
-                    <WorkGridItem id="shoesaleproject" title="Shopping Cart" thumbnail={thumbMyProject}>
+                    <WorkGridItem id="shoesaleproject" title="Shopping Cart" thumbnail={MyProject}>
                         Shopping Cart
                     </WorkGridItem>
                 </Section>
                 <Section>
-                    <WorkGridItem id="helpdesk" title="IT Helpdesk System" thumbnail={thumbMyProject2}>
+                    <WorkGridItem id="helpdesk" title="IT Helpdesk System" thumbnail={MyProject2}>
                         IT helpdesk system
                     </WorkGridItem>
                 </Section>
